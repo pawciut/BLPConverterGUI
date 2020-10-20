b8d04dc87867bb7ca2d5d1cc400e49cdecfb94b6
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2020-10-20 12:16:22 +0200

    adding sources

--- BLPConverterGUI/ManForm.Designer.cs
+++ BLPConverterGUI/ManForm.Designer.cs
@@ -31,6 +31,15 @@
             this.lbBLPPath = new System.Windows.Forms.Label();
             this.txtBLPConverterPath = new System.Windows.Forms.TextBox();
             this.btnBLTConverterPathBrowse = new System.Windows.Forms.Button();
+            this.lbSource = new System.Windows.Forms.Label();
+            this.lvSources = new System.Windows.Forms.ListView();
+            this.btnSelectSourceFolder = new System.Windows.Forms.Button();
+            this.btnSelectSourceFile = new System.Windows.Forms.Button();
+            this.btnClearSources = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lbConvertingStatus = new System.Windows.Forms.Label();
+            this.lbConvertingCount = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // lbBLPPath
@@ -61,11 +70,103 @@
             this.btnBLTConverterPathBrowse.UseVisualStyleBackColor = true;
             this.btnBLTConverterPathBrowse.Click += new System.EventHandler(this.btnBLTConverterPathBrowse_Click);
             // 
+            // lbSource
+            // 
+            this.lbSource.AutoSize = true;
+            this.lbSource.Location = new System.Drawing.Point(13, 107);
+            this.lbSource.Name = "lbSource";
+            this.lbSource.Size = new System.Drawing.Size(49, 13);
+            this.lbSource.TabIndex = 3;
+            this.lbSource.Text = "Sources:";
+            // 
+            // lvSources
+            // 
+            this.lvSources.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvSources.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvSources.HideSelection = false;
+            this.lvSources.Location = new System.Drawing.Point(12, 131);
+            this.lvSources.Name = "lvSources";
+            this.lvSources.Size = new System.Drawing.Size(733, 310);
+            this.lvSources.TabIndex = 4;
+            this.lvSources.UseCompatibleStateImageBehavior = false;
+            this.lvSources.View = System.Windows.Forms.View.Details;
+            // 
+            // btnSelectSourceFolder
+            // 
+            this.btnSelectSourceFolder.Location = new System.Drawing.Point(68, 102);
+            this.btnSelectSourceFolder.Name = "btnSelectSourceFolder";
+            this.btnSelectSourceFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectSourceFolder.TabIndex = 5;
+            this.btnSelectSourceFolder.Text = "SelectFolder";
+            this.btnSelectSourceFolder.UseVisualStyleBackColor = true;
+            this.btnSelectSourceFolder.Click += new System.EventHandler(this.btnSelectSourceFolder_Click);
+            // 
+            // btnSelectSourceFile
+            // 
+            this.btnSelectSourceFile.Location = new System.Drawing.Point(149, 102);
+            this.btnSelectSourceFile.Name = "btnSelectSourceFile";
+            this.btnSelectSourceFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectSourceFile.TabIndex = 6;
+            this.btnSelectSourceFile.Text = "Select file";
+            this.btnSelectSourceFile.UseVisualStyleBackColor = true;
+            this.btnSelectSourceFile.Click += new System.EventHandler(this.btnSelectSourceFile_Click);
+            // 
+            // btnClearSources
+            // 
+            this.btnClearSources.Location = new System.Drawing.Point(230, 102);
+            this.btnClearSources.Name = "btnClearSources";
+            this.btnClearSources.Size = new System.Drawing.Size(62, 23);
+            this.btnClearSources.TabIndex = 7;
+            this.btnClearSources.Text = "Clear";
+            this.btnClearSources.UseVisualStyleBackColor = true;
+            this.btnClearSources.Click += new System.EventHandler(this.btnClearSources_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(118, 59);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(592, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // lbConvertingStatus
+            // 
+            this.lbConvertingStatus.AutoSize = true;
+            this.lbConvertingStatus.Location = new System.Drawing.Point(124, 43);
+            this.lbConvertingStatus.Name = "lbConvertingStatus";
+            this.lbConvertingStatus.Size = new System.Drawing.Size(77, 13);
+            this.lbConvertingStatus.TabIndex = 9;
+            this.lbConvertingStatus.Text = "Converting file ";
+            // 
+            // lbConvertingCount
+            // 
+            this.lbConvertingCount.AutoSize = true;
+            this.lbConvertingCount.Location = new System.Drawing.Point(686, 85);
+            this.lbConvertingCount.Name = "lbConvertingCount";
+            this.lbConvertingCount.Size = new System.Drawing.Size(24, 13);
+            this.lbConvertingCount.TabIndex = 10;
+            this.lbConvertingCount.Text = "0/1";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File";
+            this.columnHeader1.Width = 400;
+            // 
             // ManForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(764, 453);
+            this.Controls.Add(this.lbConvertingCount);
+            this.Controls.Add(this.lbConvertingStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnClearSources);
+            this.Controls.Add(this.btnSelectSourceFile);
+            this.Controls.Add(this.btnSelectSourceFolder);
+            this.Controls.Add(this.lvSources);
+            this.Controls.Add(this.lbSource);
             this.Controls.Add(this.btnBLTConverterPathBrowse);
             this.Controls.Add(this.txtBLPConverterPath);
             this.Controls.Add(this.lbBLPPath);
@@ -81,6 +182,15 @@
         private System.Windows.Forms.Label lbBLPPath;
         private System.Windows.Forms.TextBox txtBLPConverterPath;
         private System.Windows.Forms.Button btnBLTConverterPathBrowse;
+        private System.Windows.Forms.Label lbSource;
+        private System.Windows.Forms.ListView lvSources;
+        private System.Windows.Forms.Button btnSelectSourceFolder;
+        private System.Windows.Forms.Button btnSelectSourceFile;
+        private System.Windows.Forms.Button btnClearSources;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lbConvertingStatus;
+        private System.Windows.Forms.Label lbConvertingCount;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
 
--- BLPConverterGUI/ManForm.cs
+++ BLPConverterGUI/ManForm.cs
@@ -13,12 +13,20 @@ namespace BLPConverterGUI
 {
     public partial class ManForm : Form
     {
+        FolderBrowserDialog sourceFolderBrowserDialog;
+
         public ManForm()
         {
             InitializeComponent();
 
             //Defaults
             txtBLPConverterPath.Text = @"C:\Repos\WOW2D\Tools\BLPConverter_8-4\BLPConverter.exe";
+            //initial source directory
+            sourceFolderBrowserDialog = new FolderBrowserDialog();
+            sourceFolderBrowserDialog.SelectedPath = @"C:\Repos\WOW2D\Tools\cascview_en\x64\Work";
+
+            lbConvertingStatus.Text = String.Empty;
+            lbConvertingCount.Text = String.Empty;
         }
 
         private void btnBLTConverterPathBrowse_Click(object sender, EventArgs e)
@@ -28,10 +36,10 @@ namespace BLPConverterGUI
 
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
-                openFileDialog.InitialDirectory = File.Exists(txtBLPConverterPath.Text)? txtBLPConverterPath.Text : "c:\\";
+                openFileDialog.InitialDirectory = File.Exists(txtBLPConverterPath.Text) ? txtBLPConverterPath.Text : "c:\\";
                 openFileDialog.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
                 openFileDialog.FilterIndex = 1;
-                openFileDialog.RestoreDirectory = true;
+                openFileDialog.RestoreDirectory = false;
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
@@ -40,6 +48,89 @@ namespace BLPConverterGUI
 
                     txtBLPConverterPath.Text = filePath;
                 }
+            }
+        }
+
+        private void btnSelectSourceFolder_Click(object sender, EventArgs e)
+        {
+            // Show the FolderBrowserDialog.
+            string folderPath = string.Empty;
+
+            if (sourceFolderBrowserDialog == null)
+                sourceFolderBrowserDialog = new FolderBrowserDialog();
+
+
+            if (sourceFolderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                folderPath = sourceFolderBrowserDialog.SelectedPath;
+
+                //Recursively
+                AddSourceFiles(folderPath, true);
+            }
+        }
+
+        /// <summary>
+        /// add all files from directory
+        /// </summary>
+        /// <param name="path">directory path</param>
+        /// <param name="searchChildDirectories">add files from child directiorys</param>
+        void AddSourceFiles(string path, bool searchChildDirectories)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            var blpFiles = directoryInfo.EnumerateFiles("*.blp");
+            //add files from directory
+            for (int i = 0; i < blpFiles.Count(); ++i)
+            {
+                AddSourceFile(blpFiles.ElementAt(i).FullName);
+            }
+            //add child directory files
+            var childDirectories = directoryInfo.EnumerateDirectories();
+            for (int i = 0; i < childDirectories.Count(); ++i)
+            {
+                AddSourceFiles(childDirectories.ElementAt(i).FullName, searchChildDirectories);
+            }
+        }
+
+        private void btnClearSources_Click(object sender, EventArgs e)
+        {
+            lvSources.Items.Clear();
+        }
+
+        private void btnSelectSourceFile_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            string[] filePaths;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = (sourceFolderBrowserDialog != null && Directory.Exists(sourceFolderBrowserDialog.SelectedPath)) ? sourceFolderBrowserDialog.SelectedPath : "c:\\";
+                openFileDialog.Filter = "blp files (*.blp)|*.blp|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePaths = openFileDialog.FileNames;
+                    foreach (var filePath in filePaths)
+                        AddSourceFile(filePath);
+                }
+            }
+        }
+
+        /// <summary>
+        /// add single source file
+        /// </summary>
+        /// <param name="filePath"></param>
+        void AddSourceFile(string filePath)
+        {
+            var asList = lvSources.Items.Cast<ListViewItem>();
+            if (!asList.Any(lvi => lvi.Text.ToLower() == filePath.ToLower()))
+            {
+                var item = new ListViewItem(filePath);
+                lvSources.Items.Add(item);
             }
         }
     }
