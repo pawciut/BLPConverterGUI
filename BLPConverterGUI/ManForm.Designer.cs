6261875623543becccc4abf8b80326fa8c61ec4d
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2020-10-20 11:28:15 +0200

    Initial project and setting up BLPConverter path

---
+++ BLPConverterGUI/ManForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BLPConverterGUI
+{
+    partial class ManForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbBLPPath = new System.Windows.Forms.Label();
+            this.txtBLPConverterPath = new System.Windows.Forms.TextBox();
+            this.btnBLTConverterPathBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbBLPPath
+            // 
+            this.lbBLPPath.AutoSize = true;
+            this.lbBLPPath.Location = new System.Drawing.Point(12, 9);
+            this.lbBLPPath.Name = "lbBLPPath";
+            this.lbBLPPath.Size = new System.Drawing.Size(100, 13);
+            this.lbBLPPath.TabIndex = 0;
+            this.lbBLPPath.Text = "BLPConverter path:";
+            // 
+            // txtBLPConverterPath
+            // 
+            this.txtBLPConverterPath.Enabled = false;
+            this.txtBLPConverterPath.Location = new System.Drawing.Point(118, 6);
+            this.txtBLPConverterPath.Name = "txtBLPConverterPath";
+            this.txtBLPConverterPath.ReadOnly = true;
+            this.txtBLPConverterPath.Size = new System.Drawing.Size(592, 20);
+            this.txtBLPConverterPath.TabIndex = 1;
+            // 
+            // btnBLTConverterPathBrowse
+            // 
+            this.btnBLTConverterPathBrowse.Location = new System.Drawing.Point(716, 4);
+            this.btnBLTConverterPathBrowse.Name = "btnBLTConverterPathBrowse";
+            this.btnBLTConverterPathBrowse.Size = new System.Drawing.Size(29, 23);
+            this.btnBLTConverterPathBrowse.TabIndex = 2;
+            this.btnBLTConverterPathBrowse.Text = "...";
+            this.btnBLTConverterPathBrowse.UseVisualStyleBackColor = true;
+            this.btnBLTConverterPathBrowse.Click += new System.EventHandler(this.btnBLTConverterPathBrowse_Click);
+            // 
+            // ManForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBLTConverterPathBrowse);
+            this.Controls.Add(this.txtBLPConverterPath);
+            this.Controls.Add(this.lbBLPPath);
+            this.Name = "ManForm";
+            this.Text = "BLPConverter GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbBLPPath;
+        private System.Windows.Forms.TextBox txtBLPConverterPath;
+        private System.Windows.Forms.Button btnBLTConverterPathBrowse;
+    }
+}
+
---
+++ BLPConverterGUI/ManForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BLPConverterGUI
+{
+    public partial class ManForm : Form
+    {
+        public ManForm()
+        {
+            InitializeComponent();
+
+            //Defaults
+            txtBLPConverterPath.Text = @"C:\Repos\WOW2D\Tools\BLPConverter_8-4\BLPConverter.exe";
+        }
+
+        private void btnBLTConverterPathBrowse_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = File.Exists(txtBLPConverterPath.Text)? txtBLPConverterPath.Text : "c:\\";
+                openFileDialog.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    txtBLPConverterPath.Text = filePath;
+                }
+            }
+        }
+    }
+}
